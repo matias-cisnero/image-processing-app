9a4ccf2c50298eba1b583efe338cc1cf375bd3a8
matias-cisnero <matiascisnero2014@gmail.com>
2025-08-13 20:05:22 -0300

    pruebas de button (botones) realizadas

--- pruebas_tkinter_button.py
+++ pruebas_tkinter_button.py
@@ -9,5 +9,30 @@ ventana.config(bg="#dbeefc")
 
 #=======[Button]========
 
+# ===========================================================================================================
+# Comportamiento: [command, state]
+# ===========================================================================================================
+
+def accion(): print("¡Botón presionado!")
+tk.Button(command=accion).pack() # Esto solo lo imprime en consola
+
+tk.Button(text="No tocar", state=tk.DISABLED).pack() # Deshabilita el botón
+
+# ===========================================================================================================
+# Contenido: [text, textvariable, image, compound]
+# ===========================================================================================================
+
+"Igual que label"
+
+# ===========================================================================================================
+# Estilo y Apariencia: [font, fg, bg, width, height, bd, relief, cursor, activebackground, activeforeground]
+# ===========================================================================================================
+
+"Igual que label la mayoría"
+
+tk.Button(text="cambio de color al presionarme", activebackground="lightblue").pack()
+
+tk.Button(text="cambio de color al presionarme", activeforeground="lightblue").pack()
+
 # Iniciar el bucle de eventos
 ventana.mainloop()
---
+++ pruebas_tkinter_entry.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# Ventana principal
+ventana = tk.Tk()
+ventana.title("Mis Primeros Labels")
+ventana.geometry("600x600+300+100") # Ancho x Alto + posición desde izq + posición desde arriba
+ventana.iconbitmap("favicon.ico")
+ventana.config(bg="#dbeefc")
+
+#=======[Entry]========
+
+
+
+# Iniciar el bucle de eventos
+ventana.mainloop()
